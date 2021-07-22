e860cd215e4df50c759dfacfa91c4fd66a1d109e
Dinesh B S <dineshbs444@gmail.com>
2021-07-22 23:18:28 +0530

    Added code for blending images from different components

---
+++ image-blend-randomize.js
@@ -0,0 +1,28 @@
+var images = require("images");
+
+var folder = "components";
+var components = ["head", "shell", "legs"];
+var file_extension = ".png";
+var total_components = components.length;
+
+var randomNumbers = new Array();
+
+var num_images_per_component = 3;
+
+for(var i = 0; i < total_components; i++) {
+    var num = Math.floor((Math.random() * num_images_per_component) + 1);
+    randomNumbers.push(num.toString());
+}
+
+var blendedImage;
+
+for(var i = 0; i < total_components; i++) {
+    var imageUrl = folder + "/" + components[i] + "/" + randomNumbers[i] + file_extension;
+    if(i == 0) {
+        blendedImage = images(imageUrl);
+    } else {
+        blendedImage = blendedImage.draw(images(imageUrl), 0, 0);
+    }
+}
+
+blendedImage.save("character.png");
